2bc18aaaf5d9a484d90f5946956e3d58df69dc54
sahil koul <skoul4358@gmail.com>
2026-07-01 21:11:24 +0530

    feat: redesign 3D plane into aerodynamic jet with enclosed turbofans

--- src/components/ThreePlane.jsx
+++ src/components/ThreePlane.jsx
@@ -14,18 +14,33 @@ function Airplane() {
 
   return (
     // Base rotation to make it face towards us almost directly
-    <group rotation={[0.05, -0.15, 0.05]} scale={0.75}>
-      {/* Fuselage (compressed/shorter for a lighter look) */}
-      <mesh position={[0, 0, 0]} rotation={[Math.PI / 2, 0, 0]}>
-        <capsuleGeometry args={[0.8, 2.2, 32, 32]} />
-        <meshStandardMaterial color="#cbd5e1" roughness={0.2} metalness={0.6} />
-      </mesh>
+    <group rotation={[0.05, -0.2, 0.05]} scale={0.75}>
+      {/* AERODYNAMIC FUSELAGE */}
+      <group position={[0, 0, 0]}>
+        {/* Main Body (Sleek Cylinder) */}
+        <mesh position={[0, 0, -0.5]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.7, 0.7, 3.5, 32]} />
+          <meshStandardMaterial color="#cbd5e1" roughness={0.2} metalness={0.7} />
+        </mesh>
+        
+        {/* Nose Cone (Aerodynamic) */}
+        <mesh position={[0, 0, 1.25]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.05, 0.7, 1.5, 32]} />
+          <meshStandardMaterial color="#94a3b8" roughness={0.2} metalness={0.7} />
+        </mesh>
+
+        {/* Tail Cone */}
+        <mesh position={[0, 0, -2.75]} rotation={[-Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.2, 0.7, 1.0, 32]} />
+          <meshStandardMaterial color="#cbd5e1" roughness={0.2} metalness={0.7} />
+        </mesh>
+      </group>
       
       {/* Cockpit Window Glass */}
-      <mesh position={[0, 0.5, 0.6]} rotation={[0.35, 0, 0]}>
-        <sphereGeometry args={[0.55, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2.2]} />
+      <mesh position={[0, 0.5, 0.8]} rotation={[0.4, 0, 0]}>
+        <sphereGeometry args={[0.55, 32, 32, 0, Math.PI * 2, 0, Math.PI / 2.5]} />
         <meshPhysicalMaterial 
-          color="#38bdf8" 
+          color="#1e3a8a" 
           transmission={0.9} 
           opacity={1} 
           transparent 
@@ -35,7 +50,7 @@ function Airplane() {
       </mesh>
 
       {/* Pilot */}
-      <group position={[0, 0.15, 0.7]}>
+      <group position={[0, 0.15, 0.8]}>
         {/* Body */}
         <mesh position={[0, -0.15, 0]}>
           <cylinderGeometry args={[0.25, 0.3, 0.5]} />
@@ -46,78 +61,119 @@ function Airplane() {
           <sphereGeometry args={[0.22]} />
           <meshStandardMaterial color="#fde047" roughness={0.5} />
         </mesh>
-        {/* Goggles/Visor */}
+        {/* Visor */}
         <mesh position={[0, 0.28, 0.18]}>
           <boxGeometry args={[0.35, 0.1, 0.15]} />
           <meshStandardMaterial color="#0f172a" roughness={0.1} metalness={0.8} />
         </mesh>
-        {/* Scarf */}
-        <mesh position={[0.2, 0.1, -0.3]} rotation={[0, 0.5, -0.2]}>
-          <boxGeometry args={[0.4, 0.05, 0.8]} />
-          <meshStandardMaterial color="#ef4444" />
+      </group>
+      
+      {/* Main Swept Wings */}
+      <group position={[0, -0.15, -0.2]}>
+        {/* Left Wing */}
+        <mesh position={[-2.2, 0, -0.2]} rotation={[0, -0.3, 0]}>
+          <boxGeometry args={[4.5, 0.1, 1.4]} />
+          <meshStandardMaterial color="#94a3b8" roughness={0.3} metalness={0.7} />
+        </mesh>
+        {/* Right Wing */}
+        <mesh position={[2.2, 0, -0.2]} rotation={[0, 0.3, 0]}>
+          <boxGeometry args={[4.5, 0.1, 1.4]} />
+          <meshStandardMaterial color="#94a3b8" roughness={0.3} metalness={0.7} />
         </mesh>
       </group>
       
-      {/* Main Wing (more compressed) */}
-      <mesh position={[0, -0.15, 0.1]}>
-        <boxGeometry args={[5.5, 0.15, 1.2]} />
-        <meshStandardMaterial color="#94a3b8" roughness={0.3} metalness={0.7} />
-      </mesh>
-      
-      {/* Tail Fin */}
-      <mesh position={[0, 0.7, -1.6]} rotation={[-0.15, 0, 0]}>
-        <boxGeometry args={[0.15, 1.2, 0.8]} />
+      {/* Swept Tail Fin */}
+      <mesh position={[0, 0.8, -2.6]} rotation={[-0.4, 0, 0]}>
+        <boxGeometry args={[0.15, 1.6, 1.2]} />
         <meshStandardMaterial color="#64748b" roughness={0.4} metalness={0.6} />
       </mesh>
       
-      {/* Horizontal Stabilizer */}
-      <mesh position={[0, 0, -1.7]}>
-        <boxGeometry args={[2.0, 0.12, 0.6]} />
-        <meshStandardMaterial color="#94a3b8" roughness={0.3} metalness={0.6} />
-      </mesh>
-      
-      {/* Engine 1 (Left) */}
-      <group position={[-1.5, -0.35, 0.4]}>
-        <mesh rotation={[Math.PI / 2, 0, 0]}>
-          <cylinderGeometry args={[0.3, 0.25, 0.9, 32]} />
-          <meshStandardMaterial color="#475569" roughness={0.4} metalness={0.8} />
+      {/* Swept Horizontal Stabilizers */}
+      <group position={[0, 0.1, -2.8]}>
+        <mesh position={[-1.0, 0, 0]} rotation={[0, -0.3, 0]}>
+          <boxGeometry args={[2.2, 0.1, 0.8]} />
+          <meshStandardMaterial color="#94a3b8" roughness={0.3} metalness={0.6} />
         </mesh>
-        <group ref={prop1Ref} position={[0, 0, 0.5]}>
-          <mesh position={[0, 0, 0]}>
-            <sphereGeometry args={[0.12, 16, 16]} />
-            <meshStandardMaterial color="#1e293b" roughness={0.2} metalness={0.9} />
-          </mesh>
-          <mesh position={[0, 0, 0]}>
-            <boxGeometry args={[1.4, 0.08, 0.02]} />
-            <meshStandardMaterial color="#e2e8f0" metalness={0.5} roughness={0.2} />
-          </mesh>
-          <mesh position={[0, 0, 0]}>
-            <boxGeometry args={[0.08, 1.4, 0.02]} />
-            <meshStandardMaterial color="#e2e8f0" metalness={0.5} roughness={0.2} />
-          </mesh>
-        </group>
+        <mesh position={[1.0, 0, 0]} rotation={[0, 0.3, 0]}>
+          <boxGeometry args={[2.2, 0.1, 0.8]} />
+          <meshStandardMaterial color="#94a3b8" roughness={0.3} metalness={0.6} />
+        </mesh>
       </group>
       
-      {/* Engine 2 (Right) */}
-      <group position={[1.5, -0.35, 0.4]}>
+      {/* Jet Engine 1 (Left) */}
+      <group position={[-1.8, -0.4, 0.2]}>
+        {/* Outer Engine Nacelle */}
         <mesh rotation={[Math.PI / 2, 0, 0]}>
-          <cylinderGeometry args={[0.3, 0.25, 0.9, 32]} />
+          <cylinderGeometry args={[0.45, 0.4, 1.6, 32]} />
           <meshStandardMaterial color="#475569" roughness={0.4} metalness={0.8} />
         </mesh>
-        <group ref={prop2Ref} position={[0, 0, 0.5]}>
+        {/* Jet Intake (Dark Inner Hole) */}
+        <mesh position={[0, 0, 0.78]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.4, 0.4, 0.05, 32]} />
+          <meshStandardMaterial color="#0f172a" roughness={0.9} />
+        </mesh>
+        {/* Inside Fan Blades */}
+        <group ref={prop1Ref} position={[0, 0, 0.76]}>
+          {/* Fan Spinner */}
           <mesh position={[0, 0, 0]}>
-            <sphereGeometry args={[0.12, 16, 16]} />
-            <meshStandardMaterial color="#1e293b" roughness={0.2} metalness={0.9} />
+            <sphereGeometry args={[0.1, 16, 16]} />
+            <meshStandardMaterial color="#94a3b8" metalness={0.8} />
           </mesh>
-          <mesh position={[0, 0, 0]}>
-            <boxGeometry args={[1.4, 0.08, 0.02]} />
-            <meshStandardMaterial color="#e2e8f0" metalness={0.5} roughness={0.2} />
-          </mesh>
-          <mesh position={[0, 0, 0]}>
-            <boxGeometry args={[0.08, 1.4, 0.02]} />
-            <meshStandardMaterial color="#e2e8f0" metalness={0.5} roughness={0.2} />
-          </mesh>
+          {/* Fan Blades (Multiple thin blades for a turbofan look) */}
+          {[...Array(6)].map((_, i) => (
+            <mesh key={i} rotation={[0, 0, (i * Math.PI) / 3]}>
+              <boxGeometry args={[0.7, 0.04, 0.02]} />
+              <meshStandardMaterial color="#64748b" metalness={0.6} />
+            </mesh>
+          ))}
         </group>
+        {/* Jet Exhaust Glow */}
+        <mesh position={[0, 0, -0.8]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.3, 0.35, 0.1, 32]} />
+          <meshBasicMaterial color="#38bdf8" transparent opacity={0.8} />
+        </mesh>
+        <mesh position={[0, 0, -0.9]}>
+          <sphereGeometry args={[0.25, 16, 16]} />
+          <meshBasicMaterial color="#7dd3fc" transparent opacity={0.6} />
+        </mesh>
+      </group>
+      
+      {/* Jet Engine 2 (Right) */}
+      <group position={[1.8, -0.4, 0.2]}>
+        {/* Outer Engine Nacelle */}
+        <mesh rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.45, 0.4, 1.6, 32]} />
+          <meshStandardMaterial color="#475569" roughness={0.4} metalness={0.8} />
+        </mesh>
+        {/* Jet Intake (Dark Inner Hole) */}
+        <mesh position={[0, 0, 0.78]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.4, 0.4, 0.05, 32]} />
+          <meshStandardMaterial color="#0f172a" roughness={0.9} />
+        </mesh>
+        {/* Inside Fan Blades */}
+        <group ref={prop2Ref} position={[0, 0, 0.76]}>
+          {/* Fan Spinner */}
+          <mesh position={[0, 0, 0]}>
+            <sphereGeometry args={[0.1, 16, 16]} />
+            <meshStandardMaterial color="#94a3b8" metalness={0.8} />
+          </mesh>
+          {/* Fan Blades */}
+          {[...Array(6)].map((_, i) => (
+            <mesh key={i} rotation={[0, 0, (i * Math.PI) / 3]}>
+              <boxGeometry args={[0.7, 0.04, 0.02]} />
+              <meshStandardMaterial color="#64748b" metalness={0.6} />
+            </mesh>
+          ))}
+        </group>
+        {/* Jet Exhaust Glow */}
+        <mesh position={[0, 0, -0.8]} rotation={[Math.PI / 2, 0, 0]}>
+          <cylinderGeometry args={[0.3, 0.35, 0.1, 32]} />
+          <meshBasicMaterial color="#38bdf8" transparent opacity={0.8} />
+        </mesh>
+        <mesh position={[0, 0, -0.9]}>
+          <sphereGeometry args={[0.25, 16, 16]} />
+          <meshBasicMaterial color="#7dd3fc" transparent opacity={0.6} />
+        </mesh>
       </group>
     </group>
   );
